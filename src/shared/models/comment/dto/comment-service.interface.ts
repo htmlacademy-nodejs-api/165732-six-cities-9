d42ddf845eec945658dd69dc437f00db4a58b64e
ruaaave <anna.kurenkova@raiffeisen.ru>
2025-03-18 23:58:30 +0300

    fix

--- src/shared/models/comment/dto/comment-service.interface.ts
+++ src/shared/models/comment/dto/comment-service.interface.ts
@@ -9,4 +9,5 @@ export interface CommentService {
   findByOfferId(offerId: string): Promise<DocumentType<CommentEntity>[]>;
   deleteByOfferId(offerId: string): Promise<Nullable<number>>;
   calculateRateByOfferId(offerId: string): Promise<number>;
+  calculateCommentsByOfferId(offerId: string): Promise<number>;
 }
